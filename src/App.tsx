4eaaf52631f47c57c4a206a60a252f7d5d6ff390
alvimargodinho <alvimargodinho@hotmail.com>
2026-07-22 21:27:22 -0300

    Novo Reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
